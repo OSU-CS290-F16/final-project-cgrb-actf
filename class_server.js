70c0af39867e633d480ea3aba0e9db442f148658
Albert Morgan <morgaalb@oregonstate.edu>
2016-11-30 16:47:58 -0800

    Updated to use views and express handlebars

--- class_server.js
+++ class_server.js
@@ -1,33 +1,39 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var Handlebars = require('handlebars');
+var exphbs = require('express-handlebars');
 var classes = require('./class_info.json');
 
 var app = express();
 var port = process.env.PORT || 8889;
 
 //Read the source of class page template and compile it with handlebars
-var classPageSource = fs.readFileSync(path.join(__dirname,'public', 'class_template.html'), 'utf8');
-var classPageTemplate = Handlebars.compile(classPageSource);
+//var classPageSource = fs.readFileSync(path.join(__dirname,'public', 'class_template.html'), 'utf8');
+//var classPageTemplate = Handlebars.compile(classPageSource);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
 
 app.use('/classes', express.static(path.join(__dirname, 'public')));
 
+app.get('/classes', function(req, res, next) {
+	res.render('index');
+});
 
 app.get('/classes/:thisClass', function(req, res, next) {
-    var class_info = classes[req.params.thisClass];
+	var class_info = classes[req.params.thisClass];
 
-    if(class_info) {
-        
-        var content = classPageTemplate(class_info);
-        res.send(content);
-
+    if(class_info) {        
+		res.render('classes', class_info)  
     } else {
-
         next();
-
     }
 });
+
+app.get('/classes/create', function(req, res,next) {
+	res.render('create');
+});
+
 
 //404
 app.get('*', function(req, res) {
