3ed8aea5dd08fcb88707302c3db25f32bb5b768b
morgaalb <morgaalb@oregonstate.edu>
2016-12-07 12:44:52 -0800

    Allow for adding of classes

--- class_server.js
+++ class_server.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 //var shellescape = require('shell-escape');
 //var child_process = require('child_process');
 var MongoClient = require('mongodb').MongoClient;
-
 var update = false;
 
 var app = express();
@@ -22,6 +21,12 @@ var mongoPassword = process.env.MONGO_PASSWORD || 'uT8NXP2djfFT9H9';
 var mongoDBName = process.env.MONGO_DB || 'cs290_morgaalb';
 var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
 var mongoDB;
+var classes;
+// Local cache of the list of classes.
+// This provides two benefits:
+// 	1. Do not have to query the DB for every page request
+// 	2. Prevents weirdness with mongo async queries
+var classCache = [];
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
@@ -35,73 +40,37 @@ app.use(bodyParser.urlencoded({
 
 handlebars.registerHelper('class-list', function(context, options) {
 	var links = "";
-	if (mongoDB.classes) {
-		mongoDB.classes.find().each(function(err, item) {
-			links += '<a href="/classes/' + item.class-code + '">' + item.class-name + '</a>';
-		});
-		return links;
-	} else {
-		return "";	
-	}
+
+	classCache.forEach(function(item) {
+		links += '<a href="/classes/' + item.code + '">' + item.name + '</a>';
+	});
+	
+	return links;
 });
 
-
-
 // Run the python script to add a new class
-app.post('/classes/create/add', function(req, res) {
+app.post('/create/add', function(req, res) {
   
   if ( !req.body || !req.body['class-code'] ) {
     res.redirect('/');
   }
 
   else {
-/*
-  // Construct the list of arguments
-    args = ['create_class'
-      ,'-c', req.body['class-code']
-      ,'-r', "'" + req.body['class-name'] + "'"
-      ,'-f', req.body['instructor-first'] 
-      ,'-l', req.body['instructor-last']
-      ,'-e', req.body.email
-      ,'-u', req.body['instructor-id']
-      ,'-d', "'" + req.body.description + "'"
-      ,'-n', 'jupyter.cgrb.oregonstate.local'
-      ,'-v', 'latest'
-      ,'-m', '8GB'
-      ,'-s', '1024']
-
-    // Escape the arguments to prevent injection attacks
-    var escapedArgs = shellescape(args);
-
-    var command = '/data/scripts/docker_python/manage_class.py ' + escapedArgs;
-
-    console.log('Creating class');
-    console.log('Executing command: ' + command);
-
-    child_process.exec(command, function(error, stdout, stderr) {
-      console.log('Command Result: ');
-      console.log(JSON.stringify({ error: error,
-        stdout: stdout,
-        stderr: stderr
-      }));
-      // on function complete re-render index
-      // to give time to build page, db info should be fine
-      update = true;
-      return res.render('index', { update: true });
-      update = false;
-    });
-  }*/
-			MongoDB.classes.insert_one({
-				"code": req.body.class-code,
-				"name": req.body.class-name,
-				"first": req.body.instructor-first,
-				"last": req.body.instructor-last,
-				"email": req.body.email,
-				"instructor-id": req.body.instructor-id,
-				"description": req.body.description
-			});		
+		newClass = {
+			"code": req.body['class-code'],
+			"name": req.body['class-name'],
+			"first": req.body['instructor-first'],
+			"last": req.body['instructor-last'],
+			"email": req.body.email,
+			"instructor-id": req.body['instructor-id'],
+			"description": req.body.description
+		}	
+		
+		var classes = mongoDB.collection('classes');
+		classes.insert(newClass);		
+		classCache.push(newClass);
+		res.redirect('/classes/' + req.body['class-code']);
 	}  
-  
 });
 
 app.get('/', function(req, res, next) {
@@ -111,18 +80,17 @@ app.get('/', function(req, res, next) {
 app.get('/classes/:thisClass', function(req, res, next) {
   //var class_info = classes[req.params.thisClass];
   
-	if(mongoDB.classes) {  
-  		var classInfo = mongoDB.classes.find({ "code": req.params.thisClass});
+	var classes = mongoDB.collection('classes')  
+  	var classInfo = classes.find({ "code": req.params.thisClass});
 
-  		if(class_info) {        
-   		res.render('classes', classInfo)  
-  		} else {
-   		next();
-	  	}
+	if(classInfo) {        
+   	res.render('classes', classInfo)  
+  	} else {
+   	next();
 	}
 });
 
-app.get('/classes/create', function(req, res,next) {
+app.get('/create', function(req, res, next) {
 	res.render('create');
 });
 
@@ -138,12 +106,24 @@ app.get('*', function(req, res) {
 
 /* Connecto to the MongoDB and start up the web server */
 MongoClient.connect(mongoURL, function (err, db) {
-  if (err) {
-    console.log("== Unable to make connection to MongoDB Database.")
-    throw err;
-  }
-  mongoDB = db;
-  app.listen(port, function () {
-    console.log("== Listening on port", port);
-  });
+  	if (err) {
+   	console.log("== Unable to make connection to MongoDB Database.")
+   	throw err;
+  	}
+  	mongoDB = db;
+
+	// initialize the class cache
+	classes = mongoDB.collection('classes')	
+
+	classes.find().each(function(err, item) {
+		if(item) {	
+			classCache.push(item);
+		} else { // cache load complete; start the server up
+			app.listen(port, function () {
+    		console.log("== Listening on port", port);
+  		});
+		
+		}
+	});
+  
 });
